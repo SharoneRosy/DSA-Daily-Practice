6242680954b482287a9719a5cd0b601bef168d05
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2023-10-11 21:52:00 +0530

    Time: 1480 ms (75.21%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,4 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+delete  A from Person A ,Person B
+where A.id>B.id and A.email=B.email
