6eb11ebcf1379247851c3a1eba0ab9eb6a5019e2
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2023-11-23 12:22:48 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest path in Undirected Graph having unit distance/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        // Code here
+        ArrayList <ArrayList<Integer>> adj = new ArrayList <> (); 
+        for ( int i=0;i<n;i++ ){
+            adj.add(new ArrayList <>());
+        }
+        for (int i=0;i<m;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        int dist[]=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
+        
+        Queue <Integer> q = new LinkedList <>();
+        boolean [] visited =new boolean [n];
+        q.add(src);
+        visited[src]=true;
+        
+        while(q.isEmpty()==false){
+            int u=q.poll();
+            for (int v: adj.get(u)){
+                if (visited[v]==false){
+                    dist[v]=dist[u]+1;
+                    visited[v]=true;
+                    q.add(v);
+                }
+            }
+        }
+        
+        for (int i=0;i<dist.length;i++){
+            if (dist[i]==Integer.MAX_VALUE)
+                dist[i]=-1;
+        }
+        return dist;
+    }
+}
