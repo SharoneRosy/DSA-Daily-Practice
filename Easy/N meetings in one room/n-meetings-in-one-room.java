285a2edf53f59caea1dfb7417bee4cf1d5151caf
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2023-11-29 08:47:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/N meetings in one room/n-meetings-in-one-room.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++)
+                start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) 
+                end[i] = Integer.parseInt(inputLine[i]);
+                
+            int ans = new Solution().maxMeetings(start, end, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        // add your code here
+        
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        
+        for(int i = 0; i < end.length; i++){
+            
+            if(hashMap.containsKey(end[i]) && start[i] > hashMap.get(end[i])){
+                hashMap.put(end[i], start[i]);
+            }
+            
+            if(!hashMap.containsKey(end[i]))
+            hashMap.put(end[i], start[i]);
+        }
+        
+        Arrays.sort(end);
+        
+        int count  = 1;
+        
+        int meetingEnd = end[0];
+        
+        
+        for(int i = 1; i < end.length; i++){
+            
+            if(hashMap.get(end[i]) > meetingEnd){
+                
+                
+                
+                count++;
+                meetingEnd = end[i];
+                
+              
+            }
+            
+            
+        }
+        
+        return count;
+        
+    }
+}
+
+
+    
