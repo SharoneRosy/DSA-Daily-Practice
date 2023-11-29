807ba87a6ba498894f9093b1e1e6eb881306ca85
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2023-11-29 11:26:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Job Sequencing Problem/job-sequencing-problem.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        int jobs = 0, res = 0;
+        Arrays.sort(arr, (a, b)-> Integer.compare(b.profit,a.profit));
+        int maxDl = 0;
+        for(Job job : arr){
+            maxDl = Math.max(maxDl, job.deadline);
+        }
+        int[] avDay = new int[maxDl+1];
+        Arrays.fill(avDay, -1);
+        for(Job job : arr){
+            int j = job.deadline;
+            while(j > 0 && avDay[j] != -1) j--;
+            if(j!=0) {
+                avDay[j] = 1;
+                jobs++;
+                res += job.profit;
+            }
+        }
+        return new int[]{jobs, res};
+    }
+
+
+}
+
+/*
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+*/
