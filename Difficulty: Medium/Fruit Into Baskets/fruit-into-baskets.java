3c008f81d5fe918fdb43ac2d9a14e580291d61c5
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-07-15 17:09:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.java
+++ Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.java
@@ -31,38 +31,25 @@ class GFG {
 
 class Solution {
     public static int totalFruits(int N, int[] fruits) {       
-
-    int left = 0;int right=0;int maxLen =0;
-    Map<Integer,Integer> mpp = new HashMap<>();
-    
-    while(right<N){
-        //putting the incoming elements into map if it is  already in the map we just update the value by 1
-        mpp.put(fruits[right],mpp.getOrDefault(fruits[right],0)+1);
         
-        //if the size of Map is greater then number of baskets then we move our window till its decreases
-        if(mpp.size()>2){
-
-
-            //as we moving the window to the right reduce the frequency of the fruit which is out of the windown now
-            mpp.put(fruits[left],mpp.get(fruits[left])-1);
-
-
-            //if any fruits frequency is zero we simply remove it
-            if(mpp.get(fruits[left])==0){
-                mpp.remove(fruits[left]);
+    int left=0,right=0,maxFruits=0,n=fruits.length;
+    HashMap<Integer,Integer>map=new HashMap<>();
+    
+    while(right<n){
+        map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+        if(map.size()>2){
+            map.put(fruits[left],map.get(fruits[left])-1);
+            if(map.get(fruits[left])==0) {
+                map.remove(fruits[left]);
             }
             left++;
         }
-
-
-        //if the map size is equal or less then number of baskets then we store maxLength of the windown
-        if(mpp.size() <=2){
-            maxLen = Math.max(maxLen,right-left+1);
+        if(map.size()<=2){
+            maxFruits=Math.max(maxFruits,right-left+1);
         }
-        
         right++;
     }
-    return maxLen;
+    return maxFruits;
     }
 }
 
